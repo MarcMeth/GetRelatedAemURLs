741f0f661b8a6de8834e0b8edd145961fe10ecd7
Marc-Andre <meth0015@algonquinlive.com>
2017-05-03 16:04:31 -0400

    Update merge.js

--- js/merge.js
+++ js/merge.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 		filename = "test_" + mYear + mYday + mHour + mMin + mSec + "-eng.html";
 		
 		//tmpFile = path + filename;
-		var final = "<p>File created (save and browse to file location to see results)";
+		var final;
 		
 		$fragURL = $fragURL.replace(/\t/g, "");	
 		
@@ -46,31 +46,44 @@ $(document).ready(function() {
 		for (i=0; i<arrFragURL.length;i++) {
 			$validUrl = true
 			$arr = arrFragURL[i];
-			UrlExists($arr, function(status){
-		  	 if(status === 404){
-		  	 	$validUrl = false	
-			    } 
-			});
+			
 			type = getUrlType($arr);
 			
 			switch (type){
 				case "live":
-					var eqq = setUrlToAem($arr,type);
+					var aemUrl = setUrlToAem($arr,type);
+					var previewUrl = getPreview(aemUrl);
+					var liveUrl = $arr;
+					var editorUrl = getEditor(aemUrl);
 					
+					var status = UrlExists(liveUrl);
 					break;
 				case "preview":
-					var eqq = setUrlToAem($arr,type);
-					
+					var aemUrl = setUrlToAem($arr,type);
+					var previewUrl = $arr;
+					var liveUrl = getPreview(aemUrl);
+					var editorUrl = getEditor(aemUrl);
+
+					var status = UrlExists(liveUrl);
 					break;
 				case "editor":
-					var eqq = setUrlToAem($arr,type);
-					
+					var aemUrl = setUrlToAem($arr,type);
+					var previewUrl = getPreview(aemUrl);
+					var liveUrl = getLive(aemUrl);
+					var editorUrl = $arr;
+
+					var status = UrlExists(liveUrl);
 					break;
 				case "aemUrl":
-					
-					
+					var aemUrl = $arr;
+					var previewUrl = getPreview(aemUrl);
+					var liveUrl = getLive(aemUrl);
+					var editorUrl = getEditor(aemUrl);
+
+					var status = UrlExists(liveUrl);
 					break;
 			}
+
 			var n = $arr.search("https://");
 			if (n==-1) $arr = "";
 			else $arr = $arr.substring(n, $arr.length);	
@@ -88,15 +101,15 @@ $(document).ready(function() {
 			$("div#error").replaceWith("<p><strong>Please enter a value in the text area below</strong></p>");
 		}
 		else {
-			final += '<p>Number of pages (including current page): <strong>' + countLinks + '</strong></p>';
+			final += '';
 			links += '</p><p><strong>All Done!</strong></p>';
 			baseTempHeader = "<!DOCTYPE html><html lang=\"en\"><head><title>File Merger</title><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /></head><body>";
 			baseTempFooter = "</body></html>";
 			links = baseTempHeader + final + links + baseTempFooter;
 			final = links;
-			$('div#replace').replaceWith( final )
+			$('div#replace').replaceWith(final)
 			
-			download(links, filename, path);
+			//download(links, filename, path);
 		}
 	});
 	
@@ -118,37 +131,37 @@ $(document).ready(function() {
 		return ddiff;
 	}
 	
-	function download(content, filename, path, contentType)
-	{
-		var ie = navigator.userAgent.match(/MSIE\s([\d.]+)/),
-		ie11 = navigator.userAgent.match(/Trident\/7.0/) && navigator.userAgent.match(/rv:11/),
-		ieEDGE = navigator.userAgent.match(/Edge/g),
-		ieVer=(ie ? ie[1] : (ie11 ? 11 : (ieEDGE ? 12 : -1)));
+	//function download(content, filename, path, contentType)
+	//{
+		//var ie = navigator.userAgent.match(/MSIE\s([\d.]+)/),
+		//ie11 = navigator.userAgent.match(/Trident\/7.0/) && navigator.userAgent.match(/rv:11/),
+		//ieEDGE = navigator.userAgent.match(/Edge/g),
+		//ieVer=(ie ? ie[1] : (ie11 ? 11 : (ieEDGE ? 12 : -1)));
 		
-		if (ie && ieVer<10) {
-			console.log("No blobs on IE version less than 10");
-			return;
-		}
-		if(!contentType) contentType = 'application/octet-stream';
+		//if (ie && ieVer<10) {
+		//	console.log("No blobs on IE version less than 10");
+		//	return;
+		//}
+		//if(!contentType) contentType = 'application/octet-stream';
+		//
+		//var blob = new Blob([content]);
 		
-		var blob = new Blob([content]);
-		
-		if (ieVer>-1) {
-			window.navigator.msSaveBlob(blob, filename); 
-		}
-		else {  
-			var a = document.createElement('a');
-			var url = window.URL.createObjectURL(blob);
-			a.href = url;
-			a.download = filename;
-			document.body.appendChild(a);
-			a.click();
-			setTimeout(function(){
-				document.body.removeChild(a);
-				window.URL.revokeObjectURL(url);  
-			}, 100);  
-		}
-	}
+		//if (ieVer>-1) {
+		//	window.navigator.msSaveBlob(blob, filename); 
+		//}
+		//else {  
+			//var a = document.createElement('a');
+		//	var url = window.URL.createObjectURL(blob);
+		///	a.href = url;
+		//	a.download = filename;
+		//	document.body.appendChild(a);
+		//	a.click();
+		//	setTimeout(function(){
+		//		document.body.removeChild(a);
+		//		window.URL.revokeObjectURL(url);  
+		//	}, 100);  
+		//}
+	//}
 	//Get type from the url.
 	function getUrlType(fragURL){
 		var types ={"live":"https:\/\/www.canada.ca\/(.*?).html", "preview":"https:\/\/canada-preview.adobecqms.net\/(.*?).html", "editor":"https:\/\/author-canada-prod.adobecqms.net\/editor.html(.*?).html", "aemUrl":"https:\/\/author-canada-prod.adobecqms.net\/sites.html(.*?)\s"};
@@ -161,22 +174,25 @@ $(document).ready(function() {
 
 		return type;
 	}
-
+    
 	function getPreview(fragURL) {
 
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://canada-preview.adobecqms.net/") + ".html";
+		return n;
 	}
 
 	function getLive(fragURL) {
 
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://www.canada.ca/") + ".html";
+		return n; 
 	}
 
 	function getEditor(fragURL) {
-
+		
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://author-canada-prod.adobecqms.net/editor.html/content/canadasite/") + ".html";
+		return n;
 	}
 
-	function getAemUrl(fragURL) {
-			
-	}
 	//If url is not AEM url set the url to it. 
 	function setUrlToAem(fragURL, type) {
 		var n,language;

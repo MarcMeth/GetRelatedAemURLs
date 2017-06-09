4f4848761ccd8c8bc52d5d2a2ce738fbd484d20e
Marc-Andre <meth0015@algonquinlive.com>
2017-06-09 09:15:26 -0400

    Update merge.js

--- js/merge.js
+++ js/merge.js
@@ -1,175 +1,234 @@
 /***********************************************************************************************/
 /*** Purpose:           Takes AEM URL’s and converts them to the actual preview paths        ***/
-/*** ASP Author:        Shao Xia                                                             ***/
 /*** JS Author:         Adam Monsour && Marc-Andre Methot                                    ***/
 /*** Date:   			2017-04-13                                                           ***/
 /*** Team:              WebOps                                                               ***/
-/*** Version:           1.02 (JS Release)                                                    ***/
+/*** Version:           1.00 (JS Release)                                                    ***/
 /***********************************************************************************************/
 
 $(document).ready(function() {
-	
+
+//Code for when the back button is hit. 
+	$("#back").click(function() {
+			$("#content").hide();
+			$("#form").show();
+			$("#back").hide();
+			$(".urlContainers").html("");
+		});
+
+//Next 4 function are simply getting the value of the textarea's(URLS) then passing them off to the openUrls function. 
+	$("#openLive").click(function() {
+			$fragURL = $('#Live').val();
+			openUrls($fragURL);
+		});
+	$("#openPreview").click(function() {
+				$fragURL = $('#Preview').val();
+				openUrls($fragURL);
+		});
+	$("#openEditor").click(function() {
+				$fragURL = $('#Editor').val();
+				openUrls($fragURL);
+		});
+	$("#openAem").click(function() {
+				$fragURL = $('#Aem').val();
+				openUrls($fragURL);
+		});
+
 	$( "form" ).submit(function( e ) {
 		//Cancels default submission
 		e.preventDefault();
 		//Initialize variables
-		var filename, path, fs, tmpFile, fragURL, propURL, baseTempHeader, baseTempFooter, countLinks, validUrl;
+		var valid, $arr, arrFragURL;;
 		//Get the textarea's value
-        //var url = {originalPath:, regex:, preview:, editor:, live:};
         $fragURL = $('.fragURL').val();
-		countLinks = 1;
-		
-		// create Job number and file names
-		var d, mYear, mYday, mHour, mMin, mSec, $arr, arrFragURL;
-		
-		var d = new Date();
-		mHour = addZero(d.getHours());
-		mMin = addZero(d.getMinutes());
-		mSec = addZero(d.getSeconds());
-		mYday = dayofyear(d);
-		mYear = d.getFullYear() - 2000;
-
-		path = "links/";
-		//create the filename using the date, hour, etc.
-		filename = "test_" + mYear + mYday + mHour + mMin + mSec + "-eng.html";
-		
-		//tmpFile = path + filename;
-		var final = "<p>File created (save and browse to file location to see results)";
 		
 		$fragURL = $fragURL.replace(/\t/g, "");	
-		
 		var regex=/\r\n|\n\r|\n|\r/g;
 		arrFragURL=$fragURL.replace(regex,"\n").split("\n");
 		
-		var links = '<p>';
+		//var links = '<p>';
 		
 		for (i=0; i<arrFragURL.length;i++) {
-			$validUrl = true
 			$arr = arrFragURL[i];
-			UrlExists($arr, function(status){
-		  	 if(status === 404){
-		  	 	$validUrl = false	
-			    } 
-			});
-			type = getUrlType($arr);
 			
+			$arr = $arr.trim();
 			var n = $arr.search("https://");
 			if (n==-1) $arr = "";
-			else $arr = $arr.substring(n, $arr.length);	
+			else $arr = $arr.substring(n, $arr.length);	                   //wow
 			if ($arr != "" && $arr.indexOf('.html') !== -1) {
-				countLinks = countLinks + 1;
-				if ($validUrl === true) {
-				links += '<a href="' + $arr + '">' + $arr + '</a><br />';
-				}else if ($validUrl === false) {
-				links += '<a href="' + $arr + '">' + $arr + '</a><br />';	
-				}
-			}
-		}
+				
+				var type = getUrlType($arr);
 
-		if (countLinks == 1) {
-			$("div#error").replaceWith( "<p><strong>Please enter a value in the text area below</strong></p>");
-		}
-		else {
-			final += '<p>Number of pages (including current page): <strong>' + countLinks + '</strong></p>';
-			links += '</p><p><strong>All Done!</strong></p>';
-			baseTempHeader = "<!DOCTYPE html><html lang=\"en\"><head><title>File Merger</title><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /></head><body>";
-			baseTempFooter = "</body></html>";
-			links = baseTempHeader + final + links + baseTempFooter;
-			final = links;
-			$('div#replace').replaceWith( final )
-			
-			download(links, filename, path);
-		}
+				if (type === "type is not defined") {$("#error").html("<p>Please make sure the Url(s) you are inputing are valid aem Url(s)(live, editor, preview or a aem path)</p>"); $("#error").show();}
+				else {
+					switch (type){
+							case "live":
+								var aemUrl = setUrlToAem($arr,type);
+								var previewUrl = getPreview(aemUrl);
+								var liveUrl = $arr;
+								var editorUrl = getEditor(aemUrl);
+								
+								var status = checkIfLinkIs404(liveUrl);
+
+								break;
+
+							case "preview":
+								var aemUrl = setUrlToAem($arr,type);
+								var previewUrl = $arr;
+								var liveUrl = getLive(aemUrl);
+								var editorUrl = getEditor(aemUrl);
+
+								var status = checkIfLinkIs404(liveUrl);
+									
+								break;
+
+							case "editor":
+								var aemUrl = setUrlToAem($arr,type);
+								var previewUrl = getPreview(aemUrl);
+								var liveUrl = getLive(aemUrl);
+								var editorUrl = $arr;
+
+								var status = checkIfLinkIs404(liveUrl);
+							
+								break;
+
+							case "aemUrl":
+								var aemUrl = $arr;
+								var previewUrl = getPreview(aemUrl);
+								var liveUrl = getLive(aemUrl);
+								var editorUrl = getEditor(aemUrl);
+
+								var status = checkIfLinkIs404(liveUrl);
+
+								break;
+							
+							}
+
+							$('#Live').append(liveUrl+"\n");
+							$('#Preview').append(previewUrl+"\n"); 
+							$('#Editor').append(editorUrl+"\n"); 
+							$('#Aem').append(aemUrl+"\n"); 
+							
+						}
+
+					}
+
+				}
+
+		$("#form").hide();
+		$("#content").show();
+		$("#back").show();
+		$( ".urlContainers" ).resizable({
+
+		  minWidth: 300
+
+		});
 	});
 	
-	function addZero(i, n) {
-		if ( n == 100 ) { if (i < 10) i = "00" + i; else if (i < 100) i = "0" + i; }
-		else { i < 10 ? i = "0" + i : ""; }
-		return i;
-	}
-	
-	function dayofyear(d) {   // d is a Date object
-		var yn = d.getFullYear();
-		var mn = d.getMonth();
-		var dn = d.getDate();
-		var d1 = new Date(yn,0,1,12,0,0); // noon on Jan. 1
-		var d2 = new Date(yn,mn,dn,12,0,0); // noon on input date
-		var ddiff = Math.round((d2-d1)/864e5) + 1;
-		ddiff = addZero(ddiff, 100);
+	function checkIfLinkIs404($url){
+		var returnStatus;
 		
-		return ddiff;
-	}
-	
-	function download(content, filename, path, contentType)
-	{
-		var ie = navigator.userAgent.match(/MSIE\s([\d.]+)/),
-		ie11 = navigator.userAgent.match(/Trident\/7.0/) && navigator.userAgent.match(/rv:11/),
-		ieEDGE = navigator.userAgent.match(/Edge/g),
-		ieVer=(ie ? ie[1] : (ie11 ? 11 : (ieEDGE ? 12 : -1)));
-		
-		if (ie && ieVer<10) {
-			console.log("No blobs on IE version less than 10");
-			return;
-		}
-		if(!contentType) contentType = 'application/octet-stream';
-		
-		var blob = new Blob([content]);
-		
-		if (ieVer>-1) {
-			window.navigator.msSaveBlob(blob, filename); 
-		}
-		else {  
-			var a = document.createElement('a');
-			var url = window.URL.createObjectURL(blob);
-			a.href = url;
-			a.download = filename;
-			document.body.appendChild(a);
-			a.click();
-			setTimeout(function(){
-				document.body.removeChild(a);
-				window.URL.revokeObjectURL(url);  
-			}, 100);  
-		}
-	}
-	
-	function getUrlType(fragURL){
-		var types ={"live":"https:\/\/www.canada.ca\/(.*?).html", "preview":"https:\/\/canada-preview.adobecqms.net\/(.*?).html", "editor":"https:\/\/author-canada-prod.adobecqms.net\/editor.html(.*?).html", "aemUrl":"https:\/\/author-canada-prod.adobecqms.net\/sites.html(.*?)\s"};
-		$.each(types, function(key, value){
-			var testingForType = fragURL.match(value);
-			if (testingForType != null){
-				type = key;
-			}
+		UrlExists($url, function(status){
+
+		if(status === 200){
+       		returnStatus = "200"
+	    }
+	    else if(status === 404){
+	       returnStatus = "404"
+	    }	    
 		});
 
-		return type;
+		return returnStatus;
+
 	}
 
+function openUrls(fragURL){
+	var regex=/\r\n|\n\r|\n|\r/g;
+	arrFragURL=$fragURL.replace(regex,"\n").split("\n");
+	
+	'use strict';
+
+	arrFragURL = arrFragURL.filter(function (v) {
+	  return v != '';
+	});
+	
+	for (i=0; i<arrFragURL.length;i++) {
+		window.open(arrFragURL[i]);
+	}
+
+}
+	//Get type from the url.
+	function getUrlType(fragURL){
+		var type;
+		var types ={"live":"https:\/\/www.canada.ca\/(.*?).html", "preview":"https:\/\/canada-preview.adobecqms.net\/(.*?).html", "editor":"https:\/\/author-canada-prod.adobecqms.net\/editor.html(.*?).html", "aemUrl":"https:\/\/author-canada-prod.adobecqms.net\/sites.html(.*?)\s"};
+			$.each(types, function(key, value){
+				var testingForType = fragURL.match(value);
+				if (testingForType != null){
+					type = key;
+				}
+			});
+			
+			if (type == null) type = "type is not defined";
+			
+			return type;
+			
+	}
+    
 	function getPreview(fragURL) {
 
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://canada-preview.adobecqms.net/") + ".html";
+		return n;
 	}
 
 	function getLive(fragURL) {
 
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://www.canada.ca/") + ".html";
+		return n; 
 	}
 
 	function getEditor(fragURL) {
-
+		
+		n = fragURL.replace("https://author-canada-prod.adobecqms.net/sites.html/content/canadasite/","https://author-canada-prod.adobecqms.net/editor.html/content/canadasite/") + ".html";
+		return n;
 	}
 
-	function setURL(fragURL) {
+	//If url is not AEM url set the url to it. 
+	function setUrlToAem(fragURL, type) {
+		var n,language;
 
+		switch (type){
+			case "live":
+				n = fragURL.replace(".html","");
+				n = n.replace("https://www.canada.ca", "https://author-canada-prod.adobecqms.net/sites.html/content/canadasite");
+				break;
+			case "preview":
+				n = fragURL.replace(".html","");
+				n = n.replace("https://canada-preview.adobecqms.net","https://author-canada-prod.adobecqms.net/sites.html/content/canadasite");
+				break;
+			case "editor":	
+				var nth = 0;
+				n = fragURL.replace(/.html/g, function (match, i, original) {
+				    nth++;
+				    return (nth === 2) ? "" : match;
+				});
+				n = n.replace("https://author-canada-prod.adobecqms.net/editor.html/content/canadasite","https://author-canada-prod.adobecqms.net/sites.html/content/canadasite");			
+				break;
+		}	
+		return n;
 	}
 
-	success: function UrlExists(url, cb){
-	    jQuery.ajax({
-	        url:      url,
-	        dataType: 'text',
-	        type:     'GET',
-	        complete:  function(xhr){
-	            if(typeof cb === 'function')
-	               cb.apply(this, [xhr.status]);
-	        }
-	    });
-	}
-
+	function UrlExists(url, cb){
+    jQuery.ajax({
+        url:      url,
+        dataType: 'text',
+        type:     'GET',
+        async:     false,
+        complete:  function(xhr){
+            if(typeof cb === 'function')
+               cb.apply(this, [xhr.status]);
+        }
+    });
+}
 });
+
+
